4ab10dc1b7dce096a4530ecd7a55063f56291854
DanielReisdC <reisd99@gmail.com>
2025-03-10 20:52:59 -0300

    implementando o cadastro no render

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,19 @@
-require('dotenv').config();
+require('dotenv').config();  // Carrega as variáveis do arquivo .env
 const { Sequelize } = require('sequelize');
-require('dotenv').config();
+
+// Verifica se a URL de conexão foi carregada corretamente
 console.log('DATABASE_URL:', process.env.DATABASE_URL);
 
+// Criação da instância do Sequelize com a URL de conexão
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    logging: false
+  dialect: 'postgres',  // Usando PostgreSQL
+  dialectOptions: {
+    ssl: {
+      require: true,  // Exige conexão SSL
+      rejectUnauthorized: false  // Ignora a verificação de certificado SSL
+    }
+  },
+  logging: false  // Desativa logs desnecessários
 });
+
 module.exports = sequelize;
