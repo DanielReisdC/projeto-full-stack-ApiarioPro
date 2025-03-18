33ee7d2acc34b506e7023a74834a0a653fb6aa1d
DanielReisdC <reisd99@gmail.com>
2025-03-18 19:54:19 -0300

    voltando...

--- src/api/colmeias.js
+++ src/api/colmeias.js
@@ -1,26 +1,20 @@
 const express = require("express");
 const router = express.Router();
-const {listarColmeias, cadastrarOuAtualizarColmeia  } = require("../services/colmeias");
+const { cadastrarColmeia, listarColmeias } = require("../services/colmeias");
 
 // Rota para cadastrar uma colmeia associada ao usuário
-router.post('/cadastrar', async (req, res) => {
+router.post("/cadastrar", async (req, res) => {
   try {
-    const { usuarioId, tipo_colmeia, quantidade, estado } = req.body;
+    const { tipo, quantidade, estado, usuarioId } = req.body;
 
-    if (!usuarioId || !tipo_colmeia || !quantidade || !estado) {
-      return res.status(400).json({ mensagem: "Preencha todos os campos corretamente." });
+    if (!tipo || !quantidade || !estado || !usuarioId) {
+      return res.status(400).json({ mensagem: "Preencha todos os campos." });
     }
 
-    const resultado = await cadastrarOuAtualizarColmeia(usuarioId, tipo_colmeia, quantidade, estado);
-    
-    if (resultado.success) {
-      res.status(200).json({ mensagem: resultado.message });
-    } else {
-      res.status(500).json({ mensagem: "Erro ao processar a colmeia" });
-    }
+    const novaColmeia = await cadastrarColmeia(tipo, quantidade, estado, usuarioId);
+    res.status(201).json(novaColmeia);
   } catch (erro) {
-    console.error("Erro ao cadastrar ou atualizar colmeia:", erro);
-    res.status(500).json({ mensagem: "Erro ao cadastrar ou atualizar colmeia", erro: erro.message });
+    res.status(500).json({ mensagem: "Erro ao cadastrar colmeia", erro });
   }
 });
 
