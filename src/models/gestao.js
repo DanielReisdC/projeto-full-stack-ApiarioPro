113438739a6502a9b10a17b58b4f3b3ddc70fa71
DanielReisdC <reisd99@gmail.com>
2025-03-23 21:20:17 -0300

    ajuste api gestao

--- src/models/gestao.js
+++ src/models/gestao.js
@@ -1,28 +1,30 @@
-module.exports = (sequelize, DataTypes) => {
-    const Gestao = sequelize.define('Gestao', {
-        ano: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-        },
-        mes: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        florada: {
-            type: DataTypes.STRING,
-            allowNull: false,
-        },
-        quantidade_mes: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-        quantidade_florada: {
-            type: DataTypes.FLOAT,
-            allowNull: false,
-        },
-    }, {
-        tableName: 'gestao', // Evita pluralização automática
-    });
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database"); // Certifique-se de que o caminho está correto
 
-    return Gestao;
-};
+const Gestao = sequelize.define("Gestao", {
+  ano: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  mes: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  florada: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  quantidade_mes: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+  quantidade_florada: {
+    type: DataTypes.FLOAT,
+    allowNull: false,
+  },
+}, {
+  tableName: 'Gestao', // Nome da tabela com a primeira letra maiúscula
+  underscored: true, // Adiciona automaticamente o uso de snake_case (ex: quantidade_mes ao invés de quantidadeMes)
+});
+
+module.exports = Gestao;
